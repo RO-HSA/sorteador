45022bb50c4a0a5495b31729e31ecb06735d80ab
Robert <robertdahora@gmail.com>
2023-08-14 10:20:29 -0300

    various fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('form-sorteador').addEventListener('submit', function(e) {
+        const resultado = document.querySelector('.resultado')
+
         e.preventDefault();
+
+        let numeroMinimo = document.getElementById('numero-minimo').value;
+        numeroMinimo = parseInt(numeroMinimo);
 
         let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = parseInt(numeroMaximo);
+    
 
         let numeroAleatorio = Math.random() * numeroMaximo;
         numeroAleatorio = Math.floor(numeroAleatorio + 1);
-
-        document.querySelector('.resultado').style.display = 'block';
-        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        if (numeroMaximo <= numeroMinimo) {
+            const erro = document.getElementById('erro-maximo-minimo');
+            erro.style.display = 'block';
+            erro.innerText = 'Valor mínimo precisa ser maior que o maximo';
+        } else {
+            document.querySelector('.resultado').style.display = 'block';
+            document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        }
+        
     })
 })
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
